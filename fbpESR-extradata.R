3b813151125d6ab143645f309236eb3686a4875c
juliengossa <julien.gossa@gmail.com>
2022-02-14 14:15:31 +0100

    Refonte heures

--- fbpESR-extradata.R
+++ fbpESR-extradata.R
@@ -1,58 +1,27 @@
 library(tidyverse)
 
-read.extradata <- function(libellé, dir=".") {
+import_limesurvey <- function(edfile="results-survey881846.csv", dir=".") {
+  
+  extradatafile <- paste0(dir,"/extradata/extradata.csv")
+  
+  extradata <- read.csv2(extradatafile)
 
-  df <- read.csv(paste0(dir,"/extradata/extradata.csv")) %>%
+  ed <- read.csv(paste0(dir,"/extradata/",edfile)) %>%
     mutate(
       Libellé = paste0(na.omit(Universite,Universite.other.,
                             greta, greta.other.,
                             regrp, regrp.other.,
                             ecole, ecole.other.,
                             autre, autre.other.))
-    ) %>%
-    filter(Libellé == libellé)
+      ) %>%
+    mutate(
+      across(starts_with("hec."), ~ as.numeric(gsub(" ","", .x))),
+      across(starts_with("dotations."), ~ as.numeric(gsub(" ","", .x)))
+      ) %>%
+    select(Libellé,docs.chcle.:dotobs) 
   
-  if(nrow(df) ==0) {
-    warning("Etablissement non trouvé dans les données additionnelles.")
-    return(list(extradata = NULL, 
-                extradata.kpi = NULL, 
-                extradata.liens = NULL))
-  }
-
-  extradata.kpi <- 
-    bind_rows(
-      df %>% select(starts_with("hec.")) %>% 
-        mutate_all(
-          function(x) as.numeric(gsub(" ","", x))
-          #function(x) as.numeric(str_replace(x," ",""))
-        ) %>%
-        pivot_longer(
-          cols = everything(),
-          names_pattern = "hec.(20..)_(.*).",
-          names_to = c("Rentrée", "kpi")
-        ) %>%
-        mutate(kpi = paste0("heures.",kpi)),
-    
-      df %>% select(starts_with("dotations.")) %>% 
-        mutate_all(
-          function(x) as.numeric(gsub(" ","", x))
-          #function(x) as.numeric(str_replace(x," ",""))
-        ) %>%
-        pivot_longer(
-          cols = everything(),
-          names_pattern = "dotations.(20..)_(.*).",
-          names_to = c("Rentrée", "kpi"),
-          names_prefix = c("","dotations.")
-        ) %>%
-        mutate(kpi = paste0("dotations.",kpi))
-    ) %>%
-    pivot_wider(
-      names_from = kpi,
-      values_from = value)
-  
-  
-  return(list(extradata = df, 
-              extradata.kpi = extradata.kpi))
+  bind_rows(extradata,ed) %>%
+    write.csv2(extradatafile, row.names = FALSE)
 }
 
 get_extradata_array <- function(extradata, qid, twodim = FALSE) {
@@ -83,35 +52,40 @@ euro_k <- function(x) {
 
 merge_and_add_kpis <- function(fbp) {
   
-  fbp$esr <- merge(all = TRUE,
-    fbp$esr,
-    fbp$extradata.kpi) 
-  
-  if(fbp$extradata$hecunite == "Heures") {
-    hec.labels <- function(valeur) paste(round(valeur,1.0),"h")
-  } else {
-    hec.labels <- function(valeur) paste(format(round(valeur/1000,2), big.mark=" "),"k€")
-  }
+  fbp$extradata.kpi <- read.csv("extradata/fbp_extradata.csv") %>% 
+    filter(UAI == fbp$UAI) %>%
+    mutate(
+      across(Heures.HeC:Heures.HeCV,
+             ~ case_when(
+               HC.Format.HeuresEuros == "Euros" ~ as.integer(.x / 41.41),
+               TRUE ~ .x
+             )
+      )
+    ) %>%
+    mutate(Rentrée = as.character(Rentrée)) %>%
+    select(Rentrée,Heures.HeE:Dotations.vieet)
+
+  fbp$esr <- full_join(fbp$esr,fbp$extradata.kpi) 
   
   fbp$esr.pnl <- fbp$esr %>%
     transmute (
       Rentrée = Rentrée,
       UAI = UAI,
-      kpi.H.heePetu = heures.HeE / (kpi.ETU.S.cycle.1.L + kpi.ETU.S.cycle.2.M),
-      kpi.H.hctPtit = heures.HeCT / kpi.ENS.S.titulaires,
-      kpi.H.hcvPhee = 1 - heures.HeCT / heures.HeC,
-      kpi.D.ens = dotations.doten,
-      kpi.D.aapens = dotations.aapen,
-      kpi.D.rech = dotations.dotre,
-      kpi.D.aaprech = dotations.aapre,
-      kpi.D.vieetu = dotations.vieet
+      kpi.H.heePetu = Heures.HeE / (kpi.ETU.S.cycle1_L + kpi.ETU.S.cycle2_M),
+      kpi.H.hctPtit = Heures.HeCT / kpi.ENS.S.titulaires,
+      kpi.H.hcvPhee = 1 - Heures.HeCT / Heures.HeC,
+      kpi.D.ens = Dotations.doten,
+      kpi.D.aapens = Dotations.aapen,
+      kpi.D.rech = Dotations.dotre,
+      kpi.D.aaprech = Dotations.aapre,
+      kpi.D.vieetu = Dotations.vieet
     ) %>%
     pivot_longer(-c(Rentrée,UAI), names_to="kpi", values_to="valeur") %>%
     mutate(
       valeur_label = case_when(
         kpi == "kpi.H.hcvPhee" ~ scales::percent(valeur,0.1),
         startsWith(kpi, "kpi.D") ~ euro_k(valeur),
-        TRUE ~ hec.labels(valeur),
+        TRUE ~ format(valeur, big.mark=" ", digits=0),
       )
     ) %>%
     filter(!is.na(valeur)) %>%
@@ -126,14 +100,18 @@ merge_and_add_kpis <- function(fbp) {
 
 fbp_get_data <- function(libellé, rentrée = 2019, rentrée.min = 2000, dir=".") {
 
-  fbp <- read.extradata(libellé, dir)
+  fbp <- list()
   fbp$Libellé <- libellé
-  
+
   fbp$etab <- kpiESR::esr.etab %>% filter(Etablissement==libellé)
   if(nrow(fbp$etab)==0) stop("UAI non trouvé.")
   
   fbp$groupe <- as.character(fbp$etab$Groupe)
   fbp$UAI <- fbp$etab$UAI
+    
+  fbp$extradata <- read.csv2("extradata/extradata.csv") %>%
+    filter(Libellé == libellé)
+  
   
   fbp$plots <- kpiESR::kpiesr_plot_all(rentrée, fbp$UAI, fbp$groupe) 
   
@@ -141,9 +119,7 @@ fbp_get_data <- function(libellé, rentrée = 2019, rentrée.min = 2000, dir="."
   fbp$esr.pnl <- kpiESR::esr.pnl %>% filter(UAI == fbp$UAI) %>%
     filter(as.character(Rentrée) >= rentrée.min) 
   
-  if(!is.null(fbp$extradata.kpi)) {
-    fbp <- merge_and_add_kpis(fbp)
-  }
+  fbp <- merge_and_add_kpis(fbp)
   
   fbp$esr.pnl <- fbp$esr.pnl %>% 
     filter(!is.na(valeur)) %>%
